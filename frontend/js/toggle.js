2d570580a176d3f23b315c2a571e2cf3bfa2a3fe
dtulasipriya37-art <dtulasipriya37@gmail.com>
2026-06-20 10:03:22 +0530

    toggle

---
+++ frontend/js/toggle.js
@@ -0,0 +1,34 @@
+var users = [
+    {
+        "name" : "John Mark",
+        "Gender" : "Male",
+        "image" : "/image/john.png"
+    },
+    {
+        "name" : "Jane River",
+        "Gender" : "Female",
+        "image" : "/image/jane.png"
+    }
+]
+
+var curID = 0;
+function toggle(){
+    //toggle curID from 0 -> 1 & 1 -> 0
+    curID = (curID + 1) % 2
+
+    //toggle the rendered user details
+    
+    //image
+
+    var user = users[curID];
+    document.getElementById("user-img").src = user.image;
+    
+    //to do: update the name and gender
+
+    //name
+    document.getElementById("user-name").innerText = user.name;
+    //gender
+    document.getElementById("user-gender").innerText = user.Gender;
+
+
+}
